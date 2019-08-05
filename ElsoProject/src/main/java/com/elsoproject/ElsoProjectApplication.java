0f488174ff0915ea477418472e630cc6bf269d5f
sztyopu <vargasztyopu@gmail.com>
2019-08-05 23:24:07 +0200

    Sprinboot elsjo projektem

---
+++ ElsoProject/src/main/java/com/elsoproject/ElsoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.elsoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsoProjectApplication.class, args);
+	}
+
+}
