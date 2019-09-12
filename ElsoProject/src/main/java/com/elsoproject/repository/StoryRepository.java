9f35ad098d14b7fd7f839c860aa4a3fff99f6044
sztyopu <vargasztyopu@gmail.com>
2019-09-12 18:08:17 +0200

    JPA helyett sajat SQL lekerdezes, JPQL - SpringBoot II 04

--- ElsoProject/src/main/java/com/elsoproject/repository/StoryRepository.java
+++ ElsoProject/src/main/java/com/elsoproject/repository/StoryRepository.java
@@ -2,7 +2,9 @@ package com.elsoproject.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.elsoproject.domain.Story;
@@ -18,7 +20,18 @@ public interface StoryRepository extends CrudRepository<Story, Long> {
 	//SELECT * FROM STORY WHERE posted IN (SELECT max(posted) FROM story) LIMIT 1;
 	Story findFirstByOrderByPostedDesc();
 
-	Story findByTitle(String title);
+	
+    //(Sima SQL - nem kell a @Param):
+	//@Query(value="SELECT * FROM Story WHERE title = ?1 limit 1", nativeQuery = true)
+	//Story findByTitle String title);	
+	
+	//(Sima SQL - kell a @ Param)
+	//@Query(value="SELECT * FROM Story WHERE title = :title limit 1", nativeQuery = true)
+	//Story findByTitle(@Param ("title")String title);	
+	
+	//(JPQL (objektumokban gondolkodik) - kell a Param)
+	@Query(value = "SELECT s From Story s WHERE s.title = :title ")
+	Story findByTitle(@Param ("title")String title);
 
 
 
